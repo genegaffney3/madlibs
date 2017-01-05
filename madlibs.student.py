c1b4b9cb79cfaade03536bceca3c540f7f8ba309
genegaffney3 <gaffney.gene3@gmail.com>
2017-01-05 18:01:16 -0500

    Updated file for python 2

--- madlibs.student.py
+++ madlibs.student.py
@@ -2,10 +2,10 @@
 #Here, you will use input and print statements to have the user create this mad lib
 
 #Asks the user to choose the first adjective of the story
-print('Pick an adjective')
+print 'Pick an adjective'
 
 #Has the user input an adjective which gets saved as the string adj1
-adj1 = input()
+adj1 = raw_input()
 
 #***Use the story prompt below to ask the user for the remaining words to complete the story***
 
@@ -14,7 +14,7 @@ adj1 = input()
 
 
 #This is where the story for each mad lib will go
-print('Roses are ' + adj1)
-print(noun + ' are blue')
-print('I love ' + noun2)
-print('And so do you!')
+print 'Roses are ' + adj1
+print 'noun + ' are blue
+print 'I love ' + noun2
+print 'And so do you!'
